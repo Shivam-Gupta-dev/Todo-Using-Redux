515366daa372371433dbd21e55b5a2d4c2ab3a64
Shivam <snoozing44@gmail.com>
2025-06-02 16:31:25 +0530

    Updated

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { addTodo, updateTodo, setButtonState, updateVar, setplaceholderContent } from '../features/todo/todoSlice'
 
